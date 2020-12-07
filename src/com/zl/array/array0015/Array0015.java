3f6aa727f98d5e656274bf8ffb4020771936403c
CRMLF <2869083541@qq.com>
2020-12-07 13:35:46 +0800

    修改array0015

--- src/com/zl/array/array0015/Array0015.java
+++ src/com/zl/array/array0015/Array0015.java
@@ -24,15 +24,14 @@ class Solution {
 
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 1; i++) {
-            if(nums[i]>0) break;
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int l = i + 1, r = nums.length - 1;
             while (l < r) {
                 if (nums[l] + nums[i] + nums[r] == 0) {
-                    System.out.println(l + " " + i + " " + r);
-                    res.add(Arrays.asList(nums[l], nums[i], nums[r]));
-                    while (nums[l] == nums[l + 1] && l < r) l++;
-                    while (nums[r] == nums[r - 1] && l < r) r--;
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
                     l++;
                     r--;
                 } else if (nums[l] + nums[i] + nums[r] > 0) {
