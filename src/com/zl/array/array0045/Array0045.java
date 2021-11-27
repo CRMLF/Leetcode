ddbd75829508cee6ea87c7cc101a41ab3719dc96
CRMLF <2869083541@qq.com>
2021-11-27 20:26:21 +0800

    日常提交

---
+++ src/com/zl/array/array0045/Array0045.java
@@ -0,0 +1,32 @@
+package com.zl.array.array0045;
+
+/**
+ * @Description
+ * @Author ZhengLing
+ * @Date 2021/11/23 15:15
+ */
+public class Array0045 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().jump(new int[]{5, 6, 4, 4, 6, 9, 4, 4, 7, 4, 4, 8, 2, 6, 8, 1, 5, 9, 6, 5, 2, 7, 9, 7, 9, 6, 9, 4, 1, 6, 8, 8, 4, 4, 2, 0, 3, 8, 5}));
+    }
+}
+
+class Solution {
+    public int jump(int[] nums) {
+        int ans = 0;
+        int start = 0;
+        int end = 1;
+        while (end < nums.length) {
+            int maxPos = 0;
+            for (int i = start; i < end; i++) {
+                // 能跳到最远的距离
+                maxPos = Math.max(maxPos, i + nums[i]);
+            }
+            start = end;      // 下一次起跳点范围开始的格子
+            end = maxPos + 1; // 下一次起跳点范围结束的格子
+            ans++;            // 跳跃次数
+        }
+        return ans;
+    }
+
+}
