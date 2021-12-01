8e7d3d4cdce6b1c58025f5698ebf8ea00ad051d2
CRMLF <2869083541@qq.com>
2021-12-01 16:09:01 +0800

    日常提交

---
+++ src/com/zl/array/array0059/Array0059.java
@@ -0,0 +1,43 @@
+package com.zl.array.array0059;
+
+/**
+ * @Description
+ * @Author ZhengLing
+ * @Date 2021/12/01 15:46
+ */
+public class Array0059 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int l = 0, r = n - 1, t = 0, b = n - 1;
+        int[][] mat = new int[n][n];
+        int num = 1, tar = n * n;
+        while (num <= tar) {
+            for (int i = l; i <= r; i++) {
+                mat[t][i] = num++;
+                // left to right.
+            }
+            t++;
+            for (int i = t; i <= b; i++) {
+                mat[i][r] = num++;
+                // top to bottom.
+            }
+            r--;
+            for (int i = r; i >= l; i--) {
+                mat[b][i] = num++;
+                // right to left.
+            }
+            b--;
+            for (int i = b; i >= t; i--) {
+                mat[i][l] = num++;
+                // bottom to top.
+            }
+            l++;
+        }
+        return mat;
+    }
+}
