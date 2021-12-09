ea4caa3b646197dedbfb410004282b6d0d1e336f
CRMLF <2869083541@qq.com>
2021-12-09 20:50:10 +0800

    日常提交

--- src/com/zl/array/array0084/Array0084.java
+++ src/com/zl/array/array0084/Array0084.java
@@ -1,5 +1,9 @@
 package com.zl.array.array0084;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
 /**
  * @Description
  * @Author ZhengLing
@@ -7,7 +11,7 @@ package com.zl.array.array0084;
  */
 public class Array0084 {
     public static void main(String[] args) {
-        System.out.println(new Solution().largestRectangleArea(new int[]{2,4}));
+        System.out.println(new Solution().largestRectangleArea2(new int[]{2, 1, 2}));
     }
 }
 
@@ -34,5 +38,69 @@ class Solution {
         }
 
         return min;
+    }
+
+    public int largestRectangleArea2(int[] heights) {
+        int max = heights[0];
+        int len = heights.length;
+        for (int i = 0; i < len; i++) {
+            int left = i;
+            int right = i;
+            for (int j = i; j < len; j++) {
+                if (heights[j] >= heights[i]) {
+                    right = j;
+                } else {
+                    break;
+                }
+            }
+            for (int j = i; j >= 0; j--) {
+                if (heights[j] >= heights[i]) {
+                    left = j;
+                } else {
+                    break;
+                }
+            }
+            max = Math.max(max, (right - left + 1) * heights[i]);
+            System.out.println(left + " " + right + " " + heights[i]);
+        }
+        return max;
+    }
+
+    public int largestRectangleArea3(int[] heights) {
+        int max = heights[0];
+        int len = heights.length;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < len; i++) {
+            while (!stack.isEmpty() && heights[i] < heights[stack.peekLast()]) {
+                int height = heights[stack.removeLast()];
+
+                while (!stack.isEmpty() && heights[stack.peekLast()] == height) {
+                    stack.removeLast();
+                }
+                int width;
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peekLast() - 1;
+                }
+                max = Math.max(max, height * width);
+            }
+            stack.addLast(i);
+        }
+        while (!stack.isEmpty()) {
+            int height = heights[stack.removeLast()];
+
+            while (!stack.isEmpty() && heights[stack.peekLast()] == height) {
+                stack.removeLast();
+            }
+            int width;
+            if (stack.isEmpty()) {
+                width = len;
+            } else {
+                width = len - stack.peekLast() - 1;
+            }
+            max = Math.max(max, height * width);
+        }
+        return max;
     }
 }
