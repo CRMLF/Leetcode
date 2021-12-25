fb7457660bedd41b93e040cadf7cacc17fbc2d1c
CRMLF <2869083541@qq.com>
2021-12-25 23:58:01 +0800

    日常提交

---
+++ src/com/zl/array/array0167/Array0167.java
@@ -0,0 +1,20 @@
+package com.zl.array.array0167;
+
+import java.util.Arrays;
+
+/**
+ * @Description
+ * @Author ZhengLing
+ * @Date 2021/12/25 23:56
+ */
+public class Array0167 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{}, 0)));
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        return null;
+    }
+}
