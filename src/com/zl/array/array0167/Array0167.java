b61254736712e4f5b3247e9dc2044ba9c219dc15
CRMLF <2869083541@qq.com>
2021-12-26 00:02:22 +0800

    日常提交

--- src/com/zl/array/array0167/Array0167.java
+++ src/com/zl/array/array0167/Array0167.java
@@ -1,6 +1,7 @@
 package com.zl.array.array0167;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * @Description
@@ -9,12 +10,24 @@ import java.util.Arrays;
  */
 public class Array0167 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{}, 0)));
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
 }
 
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int len = numbers.length;
+        for (int i = 1; i <= len; i++) {
+            map.put(numbers[i - 1], i);
+        }
+
+        for (int i = 1; i <= len; i++) {
+            int temp = target - numbers[i - 1];
+            if (map.containsKey(temp)) {
+                return new int[]{i, map.get(temp)};
+            }
+        }
         return null;
     }
 }
